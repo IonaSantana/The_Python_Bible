03fa6d720a9d023b1cc6efd3660860d5542049c6
Ioná <ionasantana123@gmail.com>
2022-02-02 13:12:01 -0300

    format

--- hello_you.py
+++ hello_you.py
@@ -5,8 +5,8 @@ enjoy = input('E o que você gosta de fazer no seu tempo livre? ')
 
 exit = "{}, {}, {}, {}".format(name, age, city, enjoy)
 
-print(exit)
+print(exit, '\n\n')
 
-exit = "{1}, {0}, {2}, {3}".format(age, name, city, enjoy)
+exit = "Seu nome: {1}\nSua idade:{0}\nOnde você mora: {2}\nO que você ama fazer: {3}".format(age, name, city, enjoy)
 
 print(exit)
